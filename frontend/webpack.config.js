fc9f56e457cbf0f32d56ef1e54ebb76d4153f5b3
MooGoo <moogoo78@gmail.com>
2022-06-21 02:20:44 +0800

    build: nginx prod

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: '/plant',
   },
   resolve: {
     modules: [path.join(__dirname, 'src'), 'node_modules'],
